605e9901ee086524b425f19b4570fddb9d69a20b
dranjohn <dransheld@outlook.de>
2020-08-12 22:18:22 +0200

    Add central EmeraldEngine.h include

---
+++ EmeraldEngine/include/EmeraldEngine.h
@@ -0,0 +1,6 @@
+#pragma once
+
+/*
+Entry point for the Emerald Engine
+*/
+#include "core/EntryPoint.h"
--- EmeraldEngine/src/emeraldengine_pch.cpp
+++ EmeraldEngine/src/emeraldengine_pch.cpp
@@ -1,1 +1,2 @@
+//.cpp file for the precompiled header, as required by VS
 #include "emeraldengine_pch.h"
